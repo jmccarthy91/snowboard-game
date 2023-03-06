05735a8b27dda2b0481f8b65ed6e6713a9f38995
lfnt <jmccarthy91@gmail.com>
2023-03-06 16:10:22 -0700

    Restart game on successful finish or death

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadTime = 1.5f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
-            Debug.Log("You Lose");
+            Invoke("ReloadScene", reloadTime);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+
+    [SerializeField] float reloadTime = 1.5f;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             Debug.Log("You Finished!!");
+            Invoke("ReloadScene", reloadTime);
         }
     }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
